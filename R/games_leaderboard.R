c4d5f830c771f3945154d05efed5a4be21b0e712
Eric H Ekholm <Eric_Ekholm@ccpsnet.net>
2022-09-30 15:25:29 -0400

    add games_leaderboard function

--- R/games_leaderboard.R
+++ R/games_leaderboard.R
@@ -1,11 +1,32 @@
-#TODO
-#add return_df arg that will return the final leaderboard as a dataframe
-games_leaderboard <- function(year, division, return_df = FALSE) {
+#' Games Leaderboard
+#'
+#' @description 
+#' Retrive a Crossfit Games leaderboard from the Crossfit API. Functions to further process this data are TODO
+#' 
+#' @param year The year to request data for, as an integer
+#' @param division The competition division to request data for. Currently supports "men", "women", and "teams"
+#' @param ... Additional request parameters passed to the API. Not currently used.
+#'
+#' @return 
+#' A `cfg_leaderboard` object
+#' 
+#' @export
+#'
+#' @examples \dontrun{
+#' men_22 <- games_leaderboard(2022, division = "men")
+#' }
+#'
+games_leaderboard <- function(year, division = c("men", "women", "teams"), ...) {
+
+    # check division vals
+    division <- match.arg(division)
+
     comp <- "games"
 
-    res <- make_cfg_request(competition = comp, year = year, division = division)
+    div <- unlist(cfg_divisions[names(cfg_divisions) == division])
+
+    res <- make_cfg_request(competition = comp, year = year, division = div)
 
     res
     
-    #TODO implement return_df functionality
 }
