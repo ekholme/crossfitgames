1c9f457eaf795f024a91d39e6d65ec89c978ba58
Eric Ekholm <eric.ekholm@gmail.com>
2023-08-02 16:41:00 -0400

    use S3 for extract_workout_results

--- R/individual_workout_results.R
+++ R/individual_workout_results.R
@@ -1,44 +1,46 @@
-# # create a generic to extract individual workout results
-# extract_workout_results <- R7::new_generic("extract_workout_results", "obj")
+#' Extract Workout Results
+#'
+#' @description
+#' Extract individual workout results from a `cfg_leaderboard` object
+#'
+#' @param obj A `cfg_leaderboard` object
+#'
+#' @return
+#' A 5-column tibble including
+#' \describe{
+#' \item{athlete}{Athlete name}
+#' \item{workout_num}{Workout number}
+#' \item{workout_place}{Finishing position of the athlete in the workout}
+#' \item{points}{Number of points earned in workout}
+#' \item{score}{Recorded score for workout in time, weight, reps, etc}
+#' }
+#'
+#' @name extract_workout_results
+#' @method extract_workout_results cfg_leaderboard
+#' @export extract_workout_results.cfg_leaderboard
+#' @export
+#'
+#' @examples \dontrun{
+#' women_22 <- games_leaderboard(2022, "women")
+#'
+#' indiv_results <- extract_workout_results(women_22)
+#' }
+#'
+extract_workout_results.cfg_leaderboard <- function(obj) {
+    workout_nums <- seq_len(length(obj$results$ordinals))
 
-# #' Extract Workout Results
-# #'
-# #' @description
-# #' Extract individual workout results from a `cfg_leaderboard` object
-# #'
-# #' @param obj A `cfg_leaderboard` object
-# #'
-# #' @return
-# #' A 5-column tibble including
-# #' \describe{
-# #' \item{athlete}{Athlete name}
-# #' \item{workout_num}{Workout number}
-# #' \item{workout_place}{Finishing position of the athlete in the workout}
-# #' \item{points}{Number of points earned in workout}
-# #' \item{score}{Recorded score for workout in time, weight, reps, etc}
-# #' }
-# #'
-# #' @name extract_workout_results
-# #' @export
-# #'
-# #' @examples \dontrun{
-# #' women_22 <- games_leaderboard(2022, "women")
-# #'
-# #' indiv_results <- extract_workout_results(women_22)
-# #' }
-# #'
-# R7::method(extract_workout_results, cfg_leaderboard) <- function(obj) {
+    athletes <- seq_len(length(obj$results$leaderboardRows))
 
-#     workout_nums <- seq_len(length(obj$results$ordinals))
+    iter <- expand.grid(workout_nums, athletes)
 
-#     athletes <- seq_len(length(obj$results$leaderboardRows))
+    names(iter) <- c("workout_num", "athlete")
 
-#     iter <- expand.grid(workout_nums, athletes)
+    res <- purrr::pmap_dfr(iter, ~ iterate_workouts(obj, ..1, ..2))
 
-#     names(iter) <- c("workout_num", "athlete")
+    res
+}
 
-#     res <- purrr::pmap_dfr(iter, ~iterate_workouts(obj, ..1, ..2))
-
-#     res
-
-# }
+# create generic to extract individual workout results
+extract_workout_results <- function(x) {
+    UseMethod("extract_workout_results")
+}
